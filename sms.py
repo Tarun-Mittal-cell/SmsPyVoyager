4684a5ea2e49cb6c3d9c702c0ecda9f05c93ad4d
Tarun Mittal <tm6622@g.rit.edu>
2023-05-16 03:01:09 -0400

    added sid and auth_token

--- sms.py
+++ sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client 
 
 account_sid = 'AC624b31ae15da0d25dee9e809a38e4e73'
-auth_token = '[AuthToken]'
+auth_token = 'c8849af007dc3be78ec6b13e8434bbdc'
 client = Client(account_sid, auth_token)
 
 messsage = client.messages.create(
                                 from_='+12545406571',
-                                body= 'HELLLOOOOO',
-                                to='+15856905070'
+                                body= 'I can\'t believe this work',
+                                to='+15857203057'
                             )
 
 print(messsage.sid)
